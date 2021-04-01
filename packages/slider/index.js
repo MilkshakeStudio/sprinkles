f23ace341e5678f4145ece7627103b22bce03fce
Jordan Davis <jordan@milkshake.studio>
2021-04-01 14:06:02 -0400

    slider - set x

--- packages/slider/index.js
+++ packages/slider/index.js
@@ -17,6 +17,31 @@ class Slider {
 	// ---- init ---- //
 	init(){
 		this.items = this.wrapper.querySelectorAll(this.options.itemSelector);
+
+		this.getXOffsets();
+		this.setupSlider();
+	}
+
+	// ---- setup slider ---- //
+	setupSlider(){
+		let self = this;
+
+		TweenLite.to(this.items, {
+			position: 'absolute',
+			left: function(i, el){
+				return self.xOffsets[i];
+			},
+			duration: 0.2
+		})
+	}
+
+	// ---- get x-offsets ---- //
+	getXOffsets(){
+		this.xOffsets = [0];
+		for(let i = 1; i < this.items.length; i++){
+			let x = this.xOffsets[i - 1] + this.items[i - 1].offsetWidth;
+			this.xOffsets.push(x);
+		}
 	}
 }
 
